ca1a744d245d1f79a623735af09204377f9bf7e5
FIRST_NAME LAST_NAME <cybrs36@gmail.com>
2022-06-23 17:32:58 +0800

    css

--- client (FRONT-END)/src/components/Dashboard.js
+++ client (FRONT-END)/src/components/Dashboard.js
@@ -3,12 +3,20 @@ import React, { useState } from "react";
 function Dashboard() {
     return (
     <div className="information">
-    <form action="http://localhost:3000/user/management">
-        <input type="submit" value="User Management" />
-    </form> 
-    <form action="http://localhost:3000/group/management">
-        <input type="submit" value="Group Management" />
-    </form> 
+        <div class="container">
+            <div class="row align-items-center" style={{height: '100px'}}>
+                <div class="col">
+                    <form action="http://localhost:3000/user/management">
+                        <input type="submit" value="User Management" style={{height: '50px'}}/>
+                    </form> 
+                </div>
+                <div class="col">
+                    <form action="http://localhost:3000/group/management">
+                        <input type="submit" value="Group Management" style={{height: '50px'}}/>
+                    </form> 
+                </div>
+            </div>
+        </div>
     </div> 
     )
 }
--- client (FRONT-END)/src/components/Header.js
+++ client (FRONT-END)/src/components/Header.js
@@ -5,13 +5,11 @@ import { FaHome } from "react-icons/fa";
 function Header() {
     return (
     <header className="header-bar bg-primary mb-3">
-    <div className="container d-flex flex-column flex-md-row align-items-center p-3">
+    <div className=" d-flex align-items-center p-3">
         <div className="my-0 mr-md-auto font-weight-normal">
-        <div className="container" style={{backgroundColor: '#224966'}}>
         <Link to="/" className="text-white">
-                <FaHome className="FaHome" style={{padding: '10px', fontSize: '2em', margin: '10px', marginLeft: '50px', color:'white'}}/>
+            <FaHome className="FaaHome" style={{padding: '10px', fontSize: '3em', margin: '10px', marginLeft: '50px', color:'white', backgroundColor: '#224966'}}/>
         </Link>
-        </div>
         </div>
     </div>
     </header>
