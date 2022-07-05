4dfd6fee9c935e75884946630a8c23ac26334496
FIRST_NAME LAST_NAME <cybrs36@gmail.com>
2022-07-05 16:45:03 +0800

    yue jun for mvp, completed admin assign user group

--- client (FRONT-END)/src/components/admin/GroupChecklist.js
+++ client (FRONT-END)/src/components/admin/GroupChecklist.js
@@ -1,55 +1,29 @@
 import React, { useState, useEffect } from "react"
 import Axios from "axios"
+import UserPartial from "./UserPartial";
 
 function GroupChecklist() {
     const [userList, setUserList] = useState([]);
-    const [groupArray, setGroupArray] = useState([]);
 
     useEffect (() => {
-        Axios.get("http://localhost:3001/groups/displayGroups", { withCredentials: true })
-        .then((response) => {
-            // setUsergroupList(response.data);
-            let longGroup = [];
-            response.data.forEach((data) => {longGroup.push(data.groupName)})
-            setGroupArray(longGroup)
-            // console.log(response);
-        })
-
         Axios.get("http://localhost:3001/groups/groupCheckList", { withCredentials: true })
             .then((response) => {
                 setUserList(response.data);
-                console.log(response.data);
-
-                // let longGroup = response.data.groupings; // "ADMIN,G1,G2"
-                // const GroupsinArray = longGroup.split(",");
-                // setGroupArray(GroupsinArray);
+                // console.log(response.data);
             })
-    },[] //okay very important, runs on mount
-)
+    },[] //okay very important, runs on mount   
+    )
+
+
     return (
         <div className="information">
             <h5> <span> user details </span> </h5>
 
-            {userList.map((val) => {
+            {userList.map((val) => { // <UserPartial user={val} key={val}/>}
             return (
-                <form key={val.username}>
-                    <div className="user">
-                        <p><span style={{fontWeight: "bold"}}>Username:</span> {val.username}</p>
-                        
-                        {groupArray.map((group) => {
-                            console.log(groupArray)
-                            return(
-                                <div key={group}>
-                                    <label> {group} </label>
-                                    <input type="checkbox" value={group} defaultChecked={val.groupings.split(",").includes(group)} key={val}/>
-                                </div>
-                            )
-                        })} 
-
-                        <p><span style={{fontWeight: "bold"}}>Status:</span> {val.active == 1 ? 'Active' : 'Deactivated' } </p>
-                        {/* <button style ={{"marginLeft": "450px"}} onClick= {() => {updateStatus(val.username, val.active)}}> {val.active == 1 ? 'Deactivate' : 'Activate'} </button> */}
-                    </div>
-                </form>
+                <div key={val}>
+                <UserPartial user={val}/>
+                </div>
             );
             })}
         </div> 
--- client (FRONT-END)/test.js
+++ client (FRONT-END)/test.js
@@ -1,7 +1,17 @@
 let longGroup = "ADMIN,G1,G2"
 const myArray = longGroup.split(",");
+console.log(myArray)
+var longGroupings = "";
 for (i=0; i<myArray.length; i++) {
-    console.log(myArray[i])
+    // console.log(myArray[i])
+    let groupings = myArray[i];
+    if (i<myArray.length-1) {
+        longGroupings += (groupings + ",")
+    }else{
+        longGroupings += (groupings)
+    }
+    console.log(longGroupings)
 }
+
 
 
